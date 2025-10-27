944e89e14a32c758ad46d987f7d21d7deb5f22ab
Soo Yeon Ahn <soo.yeon.ahn2002@gmail.com>
2025-10-26 17:10:57 -0700

    Temporary commit before switching branch

--- server/models/DailyChecklist.js
+++ server/models/DailyChecklist.js
@@ -1,1 +1,40 @@
 // DailyChecklist.js
+const mongoose = require("mongoose");const mongoose = require("mongoose");
+
+const dailyTaskSchema = new mongoose.Schema({
+  configId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Date,
+    required: function() { return this.isCompleted; }
+  }
+}, { _id: true }); 
+
+const dailyChecklistSchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  tasks: [dailyTaskSchema]
+}, {
+  timestamps: true 
+});
+
+dailyChecklistSchema.index({ userId: 1, date: 1 }, { unique: true });
+
+module.exports = mongoose.model('DailyChecklist', dailyChecklistSchema);
